0f099b8c1f19737354ca49cd9d1b0cf88dd4b794
Sairaj-Coder <sairajshirole2@gmail.com>
2025-08-13 18:53:37 +0530

    Add files via upload

---
+++ dsa2/pattern7.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for (int i = 1; i <= 5; i+=1)
+    {
+        //space
+        for (int j = -3;j <=9 ;j+=3)
+        {
+            cout<<" ";
+        }
+        for (int i = 1; i < count; i++)
+        {
+            /* code */
+        }
+        
+
+        
+    }
+    
+    return 0;
+}
